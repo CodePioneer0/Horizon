b8a5ccc6591e9280866cdd536fc455f8e0166000
Sayak Sen <153994681+CodePioneer0@users.noreply.github.com>
2025-08-21 16:12:13 +0530

    Refactor Firebase configuration to use environment variables

--- firebase/client.ts
+++ firebase/client.ts
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC5KbDh2PahLSjkEJsvCrGUzCmJUyVlWA0",
-  authDomain: "horizon-e476d.firebaseapp.com",
-  projectId: "horizon-e476d",
-  storageBucket: "horizon-e476d.firebasestorage.app",
-  messagingSenderId: "298186863459",
-  appId: "1:298186863459:web:cb2f1be27568697f2f1956",
-  measurementId: "G-E2KNRJ5DT2"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
